209873080f1a6d9cf57a344f048af710de6f5f81
Kitman Lai <alanljw12345@gmail.com>
2015-09-12 10:21:56 +0800

    New feature
    auto save username and password

--- login_ZJUWLAN.py
+++ login_ZJUWLAN.py
@@ -1,22 +1,49 @@
 __author__ = 'ctknight'
+# reference:http://blog.jobbole.com/77878/
 import http.cookiejar
 import urllib.request
 import urllib.parse
-# reference:http://blog.jobbole.com/77878/
-loginURL = 'https://net.zju.edu.cn/mobile5.html'
+
+
+# loginURL = 'https://net.zju.edu.cn/mobile5.html'
+# loginURL is invalid,use requestURL instead
 requestURL = 'https://net.zju.edu.cn/cgi-bin/srun_portal'
 UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36'
 # static vars
 username = ''
 password = ''
+# initiate values
 
-# for storing username and password
+try:
+    f = open('values.txt', 'r+')
+except FileNotFoundError:
+    f = open('values.txt', 'w')
+    f.close()
+# check values.txt
+
+
 def storeValues():
-    global username
-    global password
+    global username, password
+
     f = open('values.txt', 'r+')
     f.write(username + ':' + password)
 
+
+# for storing username and password
+
+def readValues():
+    global username, password
+
+    f = open('values.txt', 'r+')
+    s = f.read()
+    if s:
+        # check s
+        username, password = s.split(':')
+    else:
+        inputValues()
+
+
+# for reading stored values
 
 def inputValues(*, passwordError=False):
     global username, password
@@ -24,16 +51,19 @@ def inputValues(*, passwordError=False):
 
         username = input('请输入用户名:')
         password = input('请输入密码:')
-
+        storeValues()
     elif passwordError:
 
         username = input('请检查用户名后再次输入:')
         password = input('请检查密码后再次输入:')
+        storeValues()
     else:
         return 'unknown args'
 
+# for reading user's input
 
-inputValues()
+readValues()
+
 # input username and password
 values = {'username': username,
           'password': password,
@@ -56,4 +86,4 @@ request = urllib.request.Request(requestURL, data)
 url = opener.open(request)
 page = url.read()
 
-# open loginurl and cookie will be collected automatically by cookiejar
+# open requesturl and cookie will be collected automatically by cookiejar
